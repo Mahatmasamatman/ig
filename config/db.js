7adb54c0da992d12b157a785acf1b5eec6c5ceb7
Mahatmasamatman <ratfl33s@gmail.com>
2021-12-08 16:17:08 +0100

    Added users and auth routes. Defined the user model. User route validates data and saves user into database.

--- config/db.js
+++ config/db.js
@@ -5,13 +5,13 @@ import config from 'config';
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db);
-        console.log('MongoDB connected.');
-    } catch(err){
-        console.error(err.message);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db);
+    console.log('MongoDB connected.');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import connectDB from './config/db.js';
+import auth from './api/auth.js';
+import users from './api/users.js';
 
 connectDB();
 
 const app = express();
 
+//Init Middleware - it allows routes to access the data in req.body
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Runnnig'));
+
+// Define routes
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
