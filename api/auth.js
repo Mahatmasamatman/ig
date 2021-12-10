ee2e986cfe51b2a09228b4818e4cf9a1c6158445
Mahatmasamatman <ratfl33s@gmail.com>
2021-12-10 17:22:47 +0100

    Fixed typos.

--- api/auth.js
+++ api/auth.js
@@ -5,4 +5,4 @@ const router = express.Router();
 //@route  GET api/auth
 //@desc   Test route
 //@access Public
-export default router.get('/', (req, res) => res.send('User route'));
+export default router.get('/', (req, res) => res.send('Auth route'));
