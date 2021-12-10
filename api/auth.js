f0f49a074e84dcf97e3797b8f0467ee43536bca3
Mahatmasamatman <ratfl33s@gmail.com>
2021-12-10 18:30:05 +0100

    There is now a middleware function that handles jwt, and it will be used in all protected routes. /api/auth returns profile data if a token is supplied in the header.

--- api/auth.js
+++ api/auth.js
@@ -1,8 +1,19 @@
 import express from 'express';
 
+import auth from '../middleware/auth.js';
+import User from '../models/User.js';
+
 const router = express.Router();
 
 //@route  GET api/auth
-//@desc   Test route
+//@desc   Test route, recieves a token and returns info about users profile
 //@access Public
-export default router.get('/', (req, res) => res.send('Auth route'));
+export default router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
